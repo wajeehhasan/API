d4df93fcbc2bbbe5515bccefcc2801d231ac31e6
Syed Wajeeh <wajeeh.hasan322@gmail.com>
2018-10-06 04:25:43 -0700

    removed extra functions all under 1 function

--- firstapi.py
+++ firstapi.py
@@ -2,23 +2,24 @@ from pyfiglet import figlet_format
 from termcolor import colored
 import requests
 
-topic=input("\nlet me tell you a joke! give me a topic : ")
+
 def jokes(topic):
 	#data gathering
 	url = "https://icanhazdadjoke.com/search"
 	response = requests.get(url, 
 		headers={"Accept":"application/json"},
-		params={"term":"cat","limit":"1"}
+		params={"term":topic}
 		)
 	data=response.json()
+	print(data)
 	#end
-	return data
-def bannerel(text,clr,on_clr,att1):
 	#welcome banner
-	asci_text=figlet_format(text1)
-	clr_ascii_text=colored(asci_text, color=clr,on_color=on_clr, attrs=att1)
+	asci_text=figlet_format("DAD JOKES 3000")
+	clr_ascii_text=colored(asci_text, color="magenta",on_color="on_green", attrs=["blink"])
 	#end
-def 
-	Print(f"\nI have {} jokes on {}".format(data["total_jokes"],data["search_term"]))
+	print("\nI have {} jokes on {}".format(data["total_jokes"],data["search_term"]))
 
-jokes("cat")
+
+
+topic=input("\nlet me tell you a joke! give me a topic : ")
+jokes(topic)
