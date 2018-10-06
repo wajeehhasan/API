1416f56d3d584bddc422aeceb4d4d7b210f101be
Syed Wajeeh <wajeeh.hasan322@gmail.com>
2018-10-06 04:15:54 -0700

    addition of banner function and wellcome message

--- firstapi.py
+++ firstapi.py
@@ -2,15 +2,23 @@ from pyfiglet import figlet_format
 from termcolor import colored
 import requests
 
-
+topic=input("\nlet me tell you a joke! give me a topic : ")
 def jokes(topic):
+	#data gathering
 	url = "https://icanhazdadjoke.com/search"
 	response = requests.get(url, 
 		headers={"Accept":"application/json"},
-		params={"term":"cat"}
+		params={"term":"cat","limit":"1"}
 		)
 	data=response.json()
-	print(data)
-
+	#end
+	return data
+def bannerel(text,clr,on_clr,att1):
+	#welcome banner
+	asci_text=figlet_format(text1)
+	clr_ascii_text=colored(asci_text, color=clr,on_color=on_clr, attrs=att1)
+	#end
+def 
+	Print(f"\nI have {} jokes on {}".format(data["total_jokes"],data["search_term"]))
 
 jokes("cat")
